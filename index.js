6bfe44c383490841f9f23de219e7ff28ca039e58
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-07-15 23:27:54 +0300

    Магазин по продаже ремонтных дроидов
    Магазин по продаже ремонтных дроидов готов к открытию, осталось написать скрипт для их заказа. Объяви переменные и присвой им соответствующие значения

--- index.js
+++ index.js
@@ -15,7 +15,28 @@ const orderedQuantity = 4;
 const totalPrice = pricePerItem * orderedQuantity;
 console.log(pricePerItem * orderedQuantity); */
 
-const productName = "Droid";
+/* const productName = "Droid";
 const pricePerItem = 3500;
-const message ="You picked ${productName}, price per item is ${pricePerItem} credits";
+const message =`You picked ${productName}, price per item is ${pricePerItem} credits`;
+console.log(message); */
+
+//Магазин по продаже ремонтных дроидов готов к открытию, осталось написать скрипт 
+//для их заказа. Объяви переменные и присвой им соответствующие значения:
+
+const pricePerDroid = 800; //- цена одного дроида, значение
+const orderedQuantity = 6; //- количество дроидов в заказе, значение
+const deliveryFee = 50; // - стоимость доставки, значение
+const totalPrice = pricePerDroid * orderedQuantity + deliveryFee; // - общая сумма заказа к оплате, не забудь о стоимости доставки
+const message =`You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`; //- сообщение о состоянии заказа в формате
 console.log(message);
+/* Тесты
+Объявлена переменная orderedQuantity
+Значение переменной orderedQuantity это число 6
+Объявлена переменная pricePerDroid
+Значение переменной pricePerDroid это число 800
+Объявлена переменная deliveryFee
+Значение переменной deliveryFee это число 50
+Объявлена переменная totalPrice
+Значение переменной totalPrice это число 4850
+Объявлена переменная message
+Значение переменной message это строка "You ordered droids worth 4850 credits. Delivery (50 credits) is included in total price." */
