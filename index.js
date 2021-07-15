49581c4c3d5a3b4520db0851780b0eb600c43e7e
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-07-15 22:38:31 +0300

    123

---
+++ index.js
@@ -0,0 +1,21 @@
+/* let topSpeed = 160;
+let distance = 617.54;
+const login = "mango935";
+const isOnline = true;
+const isAdmin = false;
+console.log( typeof topSpeed );
+console.log( typeof distance );
+console.log( typeof login );
+console.log( typeof isOnline );
+console.log( typeof isAdmin );
+console.log( typeof topSpeed ); */
+
+/* const pricePerItem = 3500;
+const orderedQuantity = 4;
+const totalPrice = pricePerItem * orderedQuantity;
+console.log(pricePerItem * orderedQuantity); */
+
+const productName = "Droid";
+const pricePerItem = 3500;
+const message ="You picked ${productName}, price per item is ${pricePerItem} credits";
+console.log(message);
