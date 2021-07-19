129452c5c83d9caf3bf9a8d9491d5a58a7d789ff
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-07-19 12:56:03 +0300

    модуль 1 завдання 27-30

--- js/module-1.js
+++ js/module-1.js
@@ -205,7 +205,7 @@ console.log(checkStorage(70, 0));
 console.log(checkStorage(200, 20));
 console.log(checkStorage(200, 250));
 console.log(checkStorage(150, 0)); */
-                                   21
+                                 /*  21
 function isNumberInRange(start, end, number) {
   const isInRange = number>=start && number<=end; // Change this line
   return isInRange;
@@ -280,5 +280,82 @@ function checkPassword(password) {
 };
 console.log(checkPassword("jqueryismyjam"));
 console.log(checkPassword("angul4r1sl1f3"));
-console.log(checkPassword("r3actsux")); 
+console.log(checkPassword("r3actsux"));*/
+                                  27
+function getSubscriptionPrice(type) {
+  let price;
+  // Change code below this line
+ switch (type) { // Change this line
+    case "starter": // Change this line
+      price = 0; // Change this line
+      break;
+
+    case "professional": // Change this line
+      price = 20; // Change this line
+      break;
+
+    case "organization": // Change this line
+      price = 50; // Change this line
+      break;
+  }
+    // Change code above this line
+  return price;
+};
+console.log(getSubscriptionPrice("professional"));
+console.log(getSubscriptionPrice("organization"));
+console.log(getSubscriptionPrice("starter"));
+28
+function checkPassword(password) {
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+  let message;
+  // Change code below this line
+  switch(password){
+    case null:
+    message = 'Canceled by user!';
+    break;
+    case ADMIN_PASSWORD:
+    message = 'Welcome!';
+    break;
+    default:
+      message = 'Access denied, wrong password!';
+  }
+  // Change code above this line
+  return message;
+};
+console.log(checkPassword('mangohackzor'));
+console.log(checkPassword(null));
+console.log(checkPassword('polyhax'));
+console.log(checkPassword('jqueryismyjam')); 
+29
+function getShippingCost(country) {
+  let message;
+  // Change code below this line
+  switch(country){
+    case 'China':
+    message = "Shipping to China will cost 100 credits";
+    break;
+    case 'Chile':
+    message = "Shipping to Chile will cost 250 credits";
+    break;
+    case 'Australia':
+    message = "Shipping to Australia will cost 170 credits";
+    break;
+    case 'Jamaica':
+    message = "Shipping to Jamaica will cost 120 credits";
+    break;
+    default:
+      message = "Sorry, there is no delivery to your country";
+  }
+  // Change code above this line
+  return message;
+};
+console.log(getShippingCost("Australia"));
+console.log(getShippingCost("Germany"));
+console.log(getShippingCost("China"));
+console.log(getShippingCost("Chile"));
+console.log(getShippingCost("Jamaica"));
+console.log(getShippingCost("Sweden"));
+
+
+
 
