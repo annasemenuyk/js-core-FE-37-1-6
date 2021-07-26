8058513953801b1115075f0dd683bf9b6ea067a7
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-07-27 00:04:04 +0300

    13

--- js/module-2.js
+++ js/module-2.js
@@ -36,8 +36,8 @@ function checkAge(age) {
   console.log(checkStorage(200, 20));
   console.log(checkStorage(200, 250));
   console.log(checkStorage(150, 0)); 
- //4*/
-const fruits = ["apple", "plum", "pear", "orange"]; 
+ //4
+const fruits = ["apple", "plum", "pear", "orange"]; */
 //5
 /*const firstElement = fruits[0];
 const secondElement = fruits[1];
@@ -56,17 +56,73 @@ console.log(fruitsArrayLength);
  const lastElement = [lastElementIndex];
  console.log(lastElementIndex);
  console.log(lastElement);
- */
+ 
 9
-function getExtremeElements(array) {
+const getExtremeElements = [[1, 2, 3, 4, 5] ,
+["Earth", "Mars", "Venus"],
+["apple", "peach", "pear", "banana"]];
+
+function getExtremeElements(array){
   // Change code below this line
-const getExtremeElements = [1, 2, 3, 4, 5]
-const getExtremeElements = ["Earth", "Mars", "Venus"]
+  const firstElement = array[0];
+  const lastElement = array[array.length - 1];
+  return   [firstElement, lastElement];
+      // Change code above this line
+  };
+   console.log(getExtremeElements);
+   10
+   const words = "Mango";
+   function splitMessage(message, delimeter) {
+    let words;
+    // Change code below this line
+    words = message.split(delimeter);
+    // Change code above this line
+    return words;
+  };
+  console.log(words);
+  11
+  function calculateEngravingPrice(message, pricePerWord) {
+    // Change code below this line
+    let totalPrice;
+    totalPrice = message.split(' ').length * pricePerWord;
+    return totalPrice;
+    // Change code above this line
+ };
+ console.log(calculateEngravingPrice("JavaScript is in my blood", 10));
+ console.log(calculateEngravingPrice("JavaScript is in my blood", 20));
+ console.log(calculateEngravingPrice("Web-development is creative work", 40));
+ console.log(calculateEngravingPrice("Web-development is creative work", 20));
+ 12
+ function makeStringFromArray(array, delimeter) {
+  let string;
+  // Change code below this line
+  string = array.join(delimeter);
+
 
   // Change code above this line
+  return string;
 };
-console.log(getExtremeElements([1, 2, 3, 4, 5]));
-console.log(getExtremeElements((["Earth", "Mars", "Venus"]));
+console.log(makeStringFromArray(["Mango", "hurries", "to", "the", "train"], " "));
+console.log(makeStringFromArray(["M", "a", "n", "g", "o"], ""));
+console.log(makeStringFromArray(["top", "picks", "for", "you"], "_"));
+13
+function slugify(title) {
+  // Change code below this line
+const string = title.toLowerCase();
+const words = string.split(' ');
+const slug = words.join('-');
+return slug;
+  // Change code above this line
+};
+console.log(slugify("Arrays for begginers"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));*/
+
+
+
+
+
 
 
     
