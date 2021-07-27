6b0ce1b445df96bd4b26dfd7f2dddd122991927f
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-07-27 21:30:48 +0300

    22

--- js/module-2.js
+++ js/module-2.js
@@ -186,15 +186,49 @@ function calculateTotalPrice(order) {
 console.log(calculateTotalPrice([12, 85, 37, 4]));
 console.log(calculateTotalPrice([164, 48, 291]));
 console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
-console.log(calculateTotalPrice());*/
+console.log(calculateTotalPrice());
 21
 function findLongestWord(string) {
   // Change code below this line
-  let words = string.toLowerCase();
-  let word = words.split(' ');
-   for (let i = 0; i < word.length; i += 1)  {
-    word += word.length;}
-  return word;
+  const stringSplit = string.split(' ');
+  let longestWord = "";
+  
+   for (const word of stringSplit){
+   if (word.length > longestWord.length) {
+     longestWord = word;
+   }}
+  return longestWord;
   // Change code above this line
+}
+22
+function createArrayOfNumbers(min, max) {
+  const numbers = [];
+ // Change code below this line
+  for (let i = min; i <= max; i += 1 ){
+    numbers.push(i);
+  }
+  // Change code above this line
+  return numbers;
 };
-console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+console.log(1, 3);
+console.log(14, 17);
+console.log(29, 34);*/
+23
+function filterArray(numbers, value) {
+    // Change code below this line
+  let newnumbers[];
+  for (let i = 0; i <= value; i += 1 ){
+      newnumbers.push(i);
+  }
+  return newnumbers[];
+ // Change code above this line
+};
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+
+
+
